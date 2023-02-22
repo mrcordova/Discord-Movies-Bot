158199e79b644e6dfc61b35c9c124f7ecb283b21
mrcordova <46950394+mrcordova@users.noreply.github.com>
2023-02-21 18:20:24 -0800

    made two separator collectors in alt title

--- commands/movies-alt-titles.js
+++ commands/movies-alt-titles.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, ActionRowBuilder, ButtonStyle, Colors } = require('discord.js');
+const { SlashCommandBuilder, ActionRowBuilder, ButtonStyle, Colors, ComponentType } = require('discord.js');
 const { api_url, MOVIE_API_KEY } = require('../config.json');
 const { createEmbed, createAltListEmbed } = require('../components/embed.js');
 const axios = require('axios');
@@ -55,38 +55,27 @@ module.exports = {
 		// TODO: work on buttons and display of alt titles for choosen film.
 		const embed = createEmbed(Colors.Blue, 'Movie will apear here', 'Some description here', 'https://discord.js.org/');
 
-		const listSize = 5;
-		let currentIndex = 0;
-		let firstTime = true;
-		let prevSelected;
-
+		
+	
 		const filter = ({ user }) => interaction.user.id == user.id;
 
 		const message = await interaction.reply({ content: 'List of Movies matching your query.', filter: filter, ephemeral: true, embeds: [embed], components: [row] });
-		const collector = message.createMessageComponentCollector({ filter, idle: 30000 });
+		const selectMenuCollector = message.createMessageComponentCollector({ filter, componentType: ComponentType.StringSelect, idle: 30000 });
+		const buttonCollector = message.createMessageComponentCollector({ filter, componentType: ComponentType.Button, idle: 30000 });
 
-		collector.on(MyEvents.Collect, async i => {
-			let selected;
-			if (i.isStringSelectMenu()) {
-				selected = i.values[0];
-			}
-			if (prevSelected != selected && selected != undefined) {
-				prevSelected = selected;
-				currentIndex = 0;
-				firstTime = true;
-			}
-			if (firstTime) {
-				firstTime = false;
-			}
-			else {
-				i.customId === backId ? (currentIndex -= listSize) : (currentIndex += listSize);
-			}
+		const listSize = 5;
+		let currentIndex = 0;
+		let movie;
+		let movieTitle;
 
+		selectMenuCollector.on(MyEvents.Collect, async i => {
+			const selected = i.values[0];
+			currentIndex = 0;
 			// const movie = movieTitles.find(m => m.id == selected);
 			// https://api.themoviedb.org/3/movie/550?api_key=fa6d2f27fc88f5bea6f896c7c38a58b4&append_to_response=alternative_titles
-			const movieResponse = await axios.get(`${api_url}${movie_route}/${prevSelected}?api_key=${MOVIE_API_KEY}&append_to_response=${movie_alt}`);
-			const movie = movieResponse.data.alternative_titles;
-			const movieTitle = movieResponse.data.title;
+			const movieResponse = await axios.get(`${api_url}${movie_route}/${selected}?api_key=${MOVIE_API_KEY}&append_to_response=${movie_alt}`);
+			movie = movieResponse.data.alternative_titles;
+			movieTitle = movieResponse.data.title;
 			// const canFitOnOnePage = movie.length <= listSize;
 			const newSelectMenu = createSelectMenu('List of Movies', movieTitle, 1, options);
 
@@ -101,19 +90,50 @@ module.exports = {
 						...(currentIndex ? [backButton.setDisabled(false)] : [backButton.setDisabled(true)]),
 						// forward button if it isn't the end
 						...(currentIndex + listSize < movie.titles.length ? [forwardButton.setDisabled(false)] : [forwardButton.setDisabled(true)]),
+					] }),
+				],
+			});
+		});
+		selectMenuCollector.on(MyEvents.Dispose, i => {
+			console.log(`select menu dispose: ${i}`);
+		});
+		selectMenuCollector.on(MyEvents.Ignore, args => {
+			console.log(`select menu ignore: ${args}`);
+		});
+		selectMenuCollector.on(MyEvents.End, async (c, r) => {
+			await interaction.editReply({ content: 'Time\'s up!', components: [] });
+		});
+		buttonCollector.on(MyEvents.Collect, async i => {
+
+			i.customId === backId ? (currentIndex -= listSize) : (currentIndex += listSize);
+
+			// const movie = movieTitles.find(m => m.id == selected);
+			// https://api.themoviedb.org/3/movie/550?api_key=fa6d2f27fc88f5bea6f896c7c38a58b4&append_to_response=alternative_titles
+
+			// const canFitOnOnePage = movie.length <= listSize;
+			const altListEmbed = await createAltListEmbed(currentIndex, listSize, movie.titles);
+			const newSelectMenu = createSelectMenu('List of Movies', movieTitle, 1, options);
+			// console.log(altListEmbed);
+			await i.update({ content: 'Selected Movie:',
+				embeds: [altListEmbed],
+				components: [
+					new ActionRowBuilder().addComponents(newSelectMenu),
+					new ActionRowBuilder({ components:  [
+						// back button if it isn't the start
+						...(currentIndex ? [backButton.setDisabled(false)] : [backButton.setDisabled(true)]),
+						// forward button if it isn't the end
+						...(currentIndex + listSize < movie.titles.length ? [forwardButton.setDisabled(false)] : [forwardButton.setDisabled(true)]),
 					] })],
 			});
 		});
-
-		collector.on(MyEvents.Dispose, i => {
-			console.log(`dispose: ${i}`);
+		buttonCollector.on(MyEvents.Dispose, i => {
+			console.log(`button dispose: ${i}`);
 		});
-		collector.on(MyEvents.End, async (c, r) => {
+		buttonCollector.on(MyEvents.Ignore, args => {
+			console.log(`button ignore: ${args}`);
+		});
+		buttonCollector.on(MyEvents.End, async (c, r) => {
 			await interaction.editReply({ content: 'Time\'s up!', components: [] });
 		});
-		collector.on(MyEvents.Ignore, args => {
-			console.log(`ignore: ${args}`);
-		});
-
 	},
 };
