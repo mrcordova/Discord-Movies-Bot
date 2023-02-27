9748dbb5dfdf64627affb2bcd5cb3a71954b0225
mrcordova <46950394+mrcordova@users.noreply.github.com>
2023-02-27 09:38:11 -0800

    Added ReleaseTypes enum

--- events/DMB-Events.js
+++ events/DMB-Events.js
@@ -5,6 +5,45 @@ class MyEvents {
     static End = 'end';
   }
 
+class ReleaseTypes {
+    static Premiere = new ReleaseTypes(1)
+    static TheatricalLimited = new ReleaseTypes(2)
+    static Theatrical = new ReleaseTypes(3)
+    static Digital = new ReleaseTypes(4)
+    static Physical = new ReleaseTypes(5)
+    static TV = new ReleaseTypes(6)
 
+    constructor(value) {
+      this.value = value;
+    }
 
-  module.exports = { MyEvents };
+    get toString() {
+      switch (this.value) {
+        case ReleaseTypes.Premiere.value:
+         return 'Premiere';
+        case ReleaseTypes.TheatricalLimited.value:
+         return 'Theatrical Limited'
+        case ReleaseTypes.Theatrical.value:
+         return 'Theatrical';
+        case ReleaseTypes.Digital.value:
+         return 'Digital';
+        case ReleaseTypes.Physical.value:
+         return 'Physical';
+        case ReleaseTypes.TV.value:
+          return 'TV';
+        default:
+          console.log('season not defined')
+      }
+    }
+  }
+// console.log(new ReleaseTypes(1).toString);
+  // const ReleaseTypes = Object.freeze({
+  //   Premiere : 1,
+  //   TheatricalLimited : 2,
+  //   Theatrical : 3,
+  //   Digital : 4,
+  //   Physical : 5,
+  //   TV : 6,
+  //   })
+
+  module.exports = { MyEvents, ReleaseTypes };
