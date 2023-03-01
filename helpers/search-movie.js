bd63d0d3adc24a2b73231b9f24511f468e444784
mrcordova <46950394+mrcordova@users.noreply.github.com>
2023-02-28 16:39:09 -0800

    added comment about results to search movie func

--- helpers/search-movie.js
+++ helpers/search-movie.js
@@ -11,6 +11,12 @@ const search_movie = '/search/movie';
 // year Integer optional
 // primary_release_year Integer optional
 
+
+// These two types of API Request, search and discover, have 2 limits:
+
+// Maximum of 20 items per page
+// Maximum of 500 pages
+
 async function searchForMovie(query, language = 'en-US', region = 'US', primaryReleaseYear = 0, page = 1) {
 	return await axios.get(`${api_url}${search_movie}?api_key=${MOVIE_API_KEY}&language=${language}&region=${region}&query=${query}&page=${page}&include_adult=false&primary_release_year=${primaryReleaseYear}`);
 }
