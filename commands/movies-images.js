bcec5feb318b8e4a4f2d385296de5d95b09422be
mrcordova <46950394+mrcordova@users.noreply.github.com>
2023-03-05 14:41:41 -0800

    refactor options in movies-images.js

--- commands/movies-images.js
+++ commands/movies-images.js
@@ -8,6 +8,7 @@ const { createNoResultEmbed, createEmbed, createImageEmbed } = require('../compo
 const { MyEvents } = require('../events/DMB-Events');
 const { createSelectMenu } = require('../components/selectMenu');
 const { getEditReply, getPrivateFollowUp } = require('../helpers/get-reply');
+const { getOptionsForSelectMenu } = require('../helpers/get-options');
 // const movie_now_playing = '/movie/now_playing';
 
 // https://api.themoviedb.org/3/movie/{movie_id}/images?api_key=<<api_key>>&language=en-US
@@ -79,17 +80,12 @@ module.exports = {
 			await interaction.reply({ embeds: [createNoResultEmbed(Colors.Red, 'No Movies Found', 'Please make a new command with a different info.')], files: [file] });
 			return;
 		}
-		const options = [];
-
-		for (const movieObject of movieTitles) {
-			const description = movieObject.overview.slice(0, 50);
-			options.push({ label: `${movieObject.title.slice(0, 81)} (${movieObject.release_date})`, description: `${description}...`, value: `${movieObject.id}` });
-		}
+		const options = getOptionsForSelectMenu(movieTitles, language);
 
 		const selectMenu = createSelectMenu('List of Movies', 'Choose an option', 1, options);
 		const row = new ActionRowBuilder().addComponents(selectMenu);
 
-		const embed = createEmbed(Colors.Blue, 'Movie will apear here', 'Some description here', 'https://discord.js.org/');
+		const embed = createEmbed(Colors.Blue, 'Movie will appear here', 'Some description here', 'https://discord.js.org/');
 
 
 		const filter = ({ user }) => interaction.user.id == user.id;
