54ca6743e5352de3e4386d34c68eabbdedee07d8
mrcordova <46950394+mrcordova@users.noreply.github.com>
2023-03-05 15:04:34 -0800

    refactor options in movies-similar.js

--- commands/movies-similar.js
+++ commands/movies-similar.js
@@ -10,6 +10,7 @@ const { createButton } = require('../components/button');
 const { getEmoji } = require('../helpers/get-emoji');
 const { createCurrencyFormatter, getProductionCompany, getCrewMember, getCast } = require('../helpers/get-production-info');
 const { getEditReply, getPrivateFollowUp } = require('../helpers/get-reply');
+const { getOptionsForSelectMenu } = require('../helpers/get-options');
 const movie_details = '/movie';
 
 
@@ -78,17 +79,12 @@ module.exports = {
 			await interaction.reply({ embeds: [createNoResultEmbed(Colors.Red, 'No Movies Found for that query', 'Please make a new command with a different options')], files:[file] });
 			return;
 		}
-		const options = [];
-
-		for (const movieObject of movieTitles) {
-			const description = movieObject.overview.slice(0, 50);
-			options.push({ label: `${movieObject.title.slice(0, 81)} (${movieObject.release_date})`, description: `${description}...`, value: `${movieObject.id}` });
-		}
+		const options = getOptionsForSelectMenu(movieTitles, language);
 
 		const selectMenu = createSelectMenu('List of Movies', 'Choose an option', 1, options);
 		const row = new ActionRowBuilder().addComponents(selectMenu);
 
-		const embed = createEmbed(Colors.Blue, 'Movie credits will apear here', 'Some description here', 'https://discord.js.org/');
+		const embed = createEmbed(Colors.Blue, 'Movie similar will appear here', 'Some description here', 'https://discord.js.org/');
 
 
 		const filter = ({ user }) => interaction.user.id == user.id;
@@ -160,6 +156,7 @@ module.exports = {
 				const creditResponse = await axios.get(`${api_url}${movie_details}/${i.customId}?api_key=${MOVIE_API_KEY}&language=${language}&append_to_response=credits,release_dates`);
 				const movieDetails = creditResponse.data;
 
+				// TODO: chack if country is null, and default to country of production like tv-search.js
 				let movieRating;
 				try {
 					movieRating = (movieDetails.release_dates.results.find(({ iso_3166_1 }) => iso_3166_1 == region) ?? { release_dates: [{ type: 3 }] })['release_dates'].find(({ type }) => type == 3).certification ?? 'N/A';
