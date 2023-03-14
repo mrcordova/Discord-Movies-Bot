a7a50f4c22732c446d271b0cffeb5efc01d7e757
mrcordova <46950394+mrcordova@users.noreply.github.com>
2023-03-14 16:29:22 -0700

    comment out slashbuilder tv/search.js

--- functions/tv/search.js
+++ functions/tv/search.js
@@ -12,53 +12,45 @@ const { getOptionsForTvSelectMenu } = require('../../helpers/get-options');
 const tv_details = '/tv';
 
 
-// https://api.themoviedb.org/3/search/tv?api_key=<<api_key>>&language=en-US&page=1&include_adult=false
-// language string Pass a ISO 639-1 value to display translated data for the fields that support it.
-// page integer Specify which page to query. optional
-// query string Pass a text query to search. This value should be URI encoded. required
-// include_adult false optional
-// first_air_date_year integer optional
-
-
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('tv-search')
-		.setDescription('Search for tv show based on a text query.')
-		.addStringOption(option =>
-			option.setName('title')
-				.setDescription('Search for the desired TV Show.')
-				.setRequired(true))
-		.addStringOption(option =>
-			option.setName('language')
-				.setDescription('Search for the desired translation.')
-				.setAutocomplete(true))
-		.addStringOption(option =>
-			option.setName('region')
-				.setDescription('Search for the desired region.')
-				.setAutocomplete(true))
-		.addIntegerOption(option =>
-			option.setName('release-year')
-				.setDescription('Search for the desired year.')
-				.setMinValue(1800)
-				.setMaxValue(3000)),
-	async autocomplete(interaction) {
-		// handle the autocompletion response (more on how to do that below)
-		const focusedOption = interaction.options.getFocused(true);
+	// data: new SlashCommandBuilder()
+	// 	.setName('tv-search')
+	// 	.setDescription('Search for tv show based on a text query.')
+	// 	.addStringOption(option =>
+	// 		option.setName('title')
+	// 			.setDescription('Search for the desired TV Show.')
+	// 			.setRequired(true))
+	// 	.addStringOption(option =>
+	// 		option.setName('language')
+	// 			.setDescription('Search for the desired translation.')
+	// 			.setAutocomplete(true))
+	// 	.addStringOption(option =>
+	// 		option.setName('region')
+	// 			.setDescription('Search for the desired region.')
+	// 			.setAutocomplete(true))
+	// 	.addIntegerOption(option =>
+	// 		option.setName('release-year')
+	// 			.setDescription('Search for the desired year.')
+	// 			.setMinValue(1800)
+	// 			.setMaxValue(3000)),
+	// async autocomplete(interaction) {
+	// 	// handle the autocompletion response (more on how to do that below)
+	// 	const focusedOption = interaction.options.getFocused(true);
 
-		let choices;
+	// 	let choices;
 
-		if (focusedOption.name === 'language') {
-			choices = translationsCodeDict;
-		}
-		if (focusedOption.name === 'region') {
-			choices = countryDict;
-		}
+	// 	if (focusedOption.name === 'language') {
+	// 		choices = translationsCodeDict;
+	// 	}
+	// 	if (focusedOption.name === 'region') {
+	// 		choices = countryDict;
+	// 	}
 
-		const filtered = choices.filter(choice => choice.name.toLowerCase().startsWith(focusedOption.value.toLowerCase()) || choice.value.toLowerCase().startsWith(focusedOption.value.toLowerCase())).slice(0, 25);
-		await interaction.respond(
-			filtered.map(choice => ({ name: `${choice.name} (${choice.value.toUpperCase()})`, value: choice.value })),
-		);
-	},
+	// 	const filtered = choices.filter(choice => choice.name.toLowerCase().startsWith(focusedOption.value.toLowerCase()) || choice.value.toLowerCase().startsWith(focusedOption.value.toLowerCase())).slice(0, 25);
+	// 	await interaction.respond(
+	// 		filtered.map(choice => ({ name: `${choice.name} (${choice.value.toUpperCase()})`, value: choice.value })),
+	// 	);
+	// },
 	async execute(interaction) {
 
 		const query = interaction.options.getString('title');
@@ -84,8 +76,8 @@ module.exports = {
 
 		const filter = ({ user }) => interaction.user.id == user.id;
 
-		// if no film is found for certain year.
-		const message = await interaction.reply({ content: 'List of TV Shows matching your query.', ephemeral: true, embeds: [embed], components: [row] });
+
+		const message = await interaction.reply({ content: 'List of TV Shows matching your query.', ephemeral: false, embeds: [embed], components: [row] });
 		const selectMenucollector = message.createMessageComponentCollector({ filter, componentType: ComponentType.StringSelect, customId:'menu', idle: 30000 });
 
 		selectMenucollector.on(MyEvents.Collect, async i => {
@@ -94,8 +86,7 @@ module.exports = {
 
 			const tvResponse = await axios.get(`${api_url}${tv_details}/${selected}?api_key=${MOVIE_API_KEY}&language=${language}&append_to_response=aggregate_credits,content_ratings`);
 			const tv = tvResponse.data;
-			// console.log(tv.origin_country);
-			// console.log(tv.content_ratings);
+
 			let tvRating;
 			try {
 				tvRating = tv.content_ratings.results.find(({ iso_3166_1 }) => ((country && iso_3166_1 == country) || tv.origin_country.includes(iso_3166_1)))['rating'];
@@ -106,11 +97,9 @@ module.exports = {
 
 			tv.rating = tvRating;
 			const network = getProductionCompany(tv['networks']);
-			// const creators = getCrewMember(tv.credits['crew'], 'Creator');
-			// console.log(tv.aggregate_credits['crew']);
+
 			const actors = getCast(tv.aggregate_credits['cast'], 10);
 			tv.language = language;
-			// console.log(tv.credits['crew']);
 
 			const tvDetailsEmbed = createTvDetailEmbed({ user: i.user, tv, network, actors, color: Colors.Aqua });
 			const newSelectMenu = createSelectMenu('List of TV Shows', tv.name.slice(0, 81), 1, options);
@@ -122,7 +111,6 @@ module.exports = {
 				components: [new ActionRowBuilder().addComponents(newSelectMenu)],
 				files: [file],
 			});
-			// collector.resetTimer([{time: 15000}]);
 		});
 
 		selectMenucollector.on(MyEvents.Dispose, i => {
@@ -133,7 +121,6 @@ module.exports = {
 			getEditReply(interaction, r);
 		});
 		selectMenucollector.on(MyEvents.Ignore, args => {
-			// console.log(`ignore: ${args}`);
 			getPrivateFollowUp(args);
 		});
 
