0e68e1a8644c72574ce39fc0fbbe4248009f6f12
mrcordova <46950394+mrcordova@users.noreply.github.com>
2023-03-15 15:04:07 -0700

    create dict for description of cmds

--- commands/help.js
+++ commands/help.js
@@ -20,9 +20,9 @@ const commandsInfo = [
 		name: '/collection',
 		value: 'Displays information about a collection with the given title.',
 		subcmds: {
-			'images': ['<title>', '(languag)'],
-			'translation': ['<title>', '(languag)'],
-			'search': ['<title>', '(languag)'],
+			'images': ['<title>', '(language)'],
+			'translation': ['<title>', '(language)'],
+			'search': ['<title>', '(language)'],
 		},
 	},
 	{
@@ -134,6 +134,25 @@ const commandsInfo = [
 	},
 ];
 
+const optionDescDict = {
+	'<title>': 'used to specify the title of the collection for which the images are desired. It is a required option',
+	'(country)': 'used to specify the name of a specific country for which to search.',
+	'<department>': 'used to specify the desired department for which to retrieve credits',
+	'<release-type>': 'used to specify the type of release for which to search.',
+	'(video-type)': 'used to specify the type of video release for which to search. ',
+	'(site)':'used to specify the type of site for which to search',
+	'(language)': 'to specify the desired translation of the collection.',
+	'(region)': 'used to specify the desired region for which to search.',
+	'(release-year)': 'used to specify the desired release year for which to search.',
+	'(video_language)': 'used to specify the desired language for a video search.',
+	'(image_language)': 'used to specify the desired language for the image search.',
+	'(platform)': ' used to specify the desired platform for a media search.',
+	'<season>': 'used to specify the desired season number to search for.',
+	'<episode>': 'used to specify the desired episode number to search for.',
+	'(media-type)': ' used to specify the type of media (i.e., TV show or movie) to search for.',
+	'(content-type)': 'a string option used to specify the desired content availability type for a media search.',
+};
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('help')
@@ -147,28 +166,55 @@ module.exports = {
 	async execute(interaction) {
 
 		const cmdName = interaction.options.getString('command-name');
+		let embed;
 
-		const embed = await new EmbedBuilder({
-			color: Colors.Green,
-			title: 'List of Commands',
-			description: `Use ${inlineCode('/help')} followed by a command name to get more additional information on a command. For example: ${inlineCode('/help /tv')}\n${'-'.repeat(40)}`,
-			fields: await Promise.all(commandsInfo.map(async (cmd) => {
-				// const filePath = path.join(commandsPath, file);
-				// const command = require(filePath);
+		if (cmdName) {
+			const command = commandsInfo.find(({ name }) => name == cmdName);
+
+			embed = await new EmbedBuilder({
+				color: Colors.Green,
+				title: `${command.name}`,
+				description: 'Get collection info.',
+				fields: await Promise.all(Object.entries(command.subcmds).map(async (key, value) => {
+					const [cmd, options ] = key;
+					// console.log(options);
+					return {
+						name: `${inlineCode(cmd)}`,
+						value: `${options}`,
+					};
+				},
+				)),
+				timestamp: new Date(),
+				// footer: {
+				//     text: tmdbName,
+				//     icon_url: tmdbIconUrl,
+				// },
+			});
+		} else {
+			embed = await new EmbedBuilder({
+				color: Colors.Green,
+				title: 'List of Commands',
+				description: `Use ${inlineCode('/help')} followed by a command name to get more additional information on a command. For example: ${inlineCode('/help /tv')}\n${'-'.repeat(40)}`,
+				fields: await Promise.all(commandsInfo.map(async (cmd) => {
+					// const filePath = path.join(commandsPath, file);
+					// const command = require(filePath);
+	
+	
+					return {
+						name: `${inlineCode(cmd.name)}`,
+						value: `${cmd.value}`,
+					};
+				},
+				)),
+				timestamp: new Date(),
+				// footer: {
+				//     text: tmdbName,
+				//     icon_url: tmdbIconUrl,
+				// },
+			});
 
 
-				return {
-					name: `${inlineCode(cmd.name)}`,
-					value: `${cmd.value}`,
-				};
-			},
-			)),
-			timestamp: new Date(),
-			// footer: {
-			//     text: tmdbName,
-			//     icon_url: tmdbIconUrl,
-			// },
-		});
+		}
 
 
 		await interaction.reply({
