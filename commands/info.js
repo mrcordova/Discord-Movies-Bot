568d3d024fb96fc944640b45127141c566c81057
mrcordova <46950394+mrcordova@users.noreply.github.com>
2023-03-15 16:20:22 -0700

    change info to creator

--- commands/info.js
+++ commands/info.js
@@ -7,7 +7,7 @@ module.exports = {
 		const embed = new EmbedBuilder({
 			color: Colors.DarkOrange,
 			title: 'Information',
-			description: `CineBot is a Discord bot for getting information about movies, TV shows, networks, companies, collections, and people in the entertainment industry.\nIt is built using Node.js and the Discord.js library (v14.7.1).\nCineBot uses the TMDB API and JustWatch info to provide data. For more information and a list of available commands, use the ${inlineCode('/help') } command.\nCineBot was created by ${hyperlink('mrcordova', 'https://github.com/mrcordova')}.`,
+			description: `CineBot is a Discord bot for getting information about movies, TV shows, networks, companies, collections, and people in the entertainment industry.\nIt is built using Node.js and the Discord.js library (v14.7.1).\nCineBot uses the TMDB API and JustWatch info to provide data. For more information and a list of available commands, use the ${inlineCode('/help') } command.\nCineBot was created by Shazam_I_Am#0680.`,
 			fields: [],
 			timestamp: new Date(),
 		});
