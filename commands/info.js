ad801a08724528882117086be867c360af7c08be
mrcordova <46950394+mrcordova@users.noreply.github.com>
2023-03-15 16:11:32 -0700

    create info.js

---
+++ commands/info.js
@@ -0,0 +1,18 @@
+const { EmbedBuilder, Colors, SlashCommandBuilder, hyperlink, inlineCode } = require('discord.js');
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('info')
+		.setDescription('provides info about CineBot'),
+	async execute(interaction) {
+		const embed = new EmbedBuilder({
+			color: Colors.DarkOrange,
+			title: 'Information',
+			description: `CineBot is a Discord bot for getting information about movies, TV shows, networks, companies, collections, and people in the entertainment industry.\nIt is built using Node.js and the Discord.js library (v14.7.1).\nCineBot uses the TMDB API and JustWatch info to provide data. For more information and a list of available commands, use the ${inlineCode('/help') } command.\nCineBot was created by ${hyperlink('mrcordova', 'https://github.com/mrcordova')}.`,
+			fields: [],
+			timestamp: new Date(),
+		});
+		await interaction.reply({
+			embeds:[embed],
+		});
+	},
+};
