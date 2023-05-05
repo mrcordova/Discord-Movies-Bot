98965a72db99d308052416f65ee0c8f491821cc3
mrcordova <46950394+mrcordova@users.noreply.github.com>
2023-05-05 10:12:58 -0700

    remove dead comments tv popular

--- functions/tv/popular.js
+++ functions/tv/popular.js
@@ -22,35 +22,7 @@ const forwardButton = createButton('Next', ButtonStyle.Secondary, forwardId, 'âž
 
 
 module.exports = {
-	// data: new SlashCommandBuilder()
-	// 	.setName('tv-popular')
-	// 	.setDescription('Get a list of the current popular TV shows on TMDB. This list updates daily.')
-	// 	.addStringOption(option =>
-	// 		option.setName('language')
-	// 			.setDescription('Search for the desired language.')
-	// 			.setMinLength(2)
-	// 			.setAutocomplete(true))
-	// 	.addStringOption(option =>
-	// 		option.setName('region')
-	// 			.setDescription('Search for the desired region.')
-	// 			.setAutocomplete(true)),
-	// async autocomplete(interaction) {
-	// 	// handle the autocompletion response (more on how to do that below)
-	// 	const focusedOption = interaction.options.getFocused(true);
-	// 	let choices;
 
-	// 	if (focusedOption.name === 'language') {
-	// 		choices = translationsCodeDict;
-	// 	}
-	// 	if (focusedOption.name === 'region') {
-	// 		choices = countryDict;
-	// 	}
-
-	// 	const filtered = choices.filter(choice => choice.name.toLowerCase().startsWith(focusedOption.value.toLowerCase()) || choice.value.toLowerCase().startsWith(focusedOption.value.toLowerCase())).slice(0, 25);
-	// 	await interaction.respond(
-	// 		filtered.map(choice => ({ name: `${choice.name} (${choice.value.toUpperCase()})`, value: choice.value })),
-	// 	);
-	// },
 	async execute(interaction) {
 
 		// TODO: added buttons for each button on list and direct them to chossen media
