6440b9d8294f0013eaa4d873f9d9d53a1d7b32e4
mrcordova <46950394+mrcordova@users.noreply.github.com>
2023-05-05 10:06:59 -0700

    remove dead comments tv episode credits

--- functions/tv/episode-credits.js
+++ functions/tv/episode-credits.js
@@ -21,52 +21,7 @@ const backButton = createButton('Previous', ButtonStyle.Secondary, backId, '⬅�
 const forwardButton = createButton('Next', ButtonStyle.Secondary, forwardId, '➡️');
 
 module.exports = {
-	// data: new SlashCommandBuilder()
-	// 	.setName('tv-episode-credits')
-	// 	.setDescription('Search for a tv episode\'s cast and crew')
-	// 	.addStringOption(option =>
-	// 		option.setName('title')
-	// 			.setDescription('Search for the desired tv episode.')
-	// 			.setRequired(true))
-	// 	.addIntegerOption(option =>
-	// 		option.setName('season')
-	// 			.setDescription('Search for the desired season.')
-	// 			.setRequired(true))
-	// 	.addIntegerOption(option =>
-	// 		option.setName('episode')
-	// 			.setDescription('Search for the desired episode.')
-	// 			.setRequired(true))
-	// 	.addStringOption(option =>
-	// 		option.setName('department')
-	// 			.setDescription('Choose desired dept.')
-	// 			.setChoices(
-	// 				...depts.reduce((arry, dept) => {
-	// 					arry.push({ name: dept, value: dept });
-	// 					return arry;
-	// 				}, []))
-	// 			.setRequired(true))
-	// 	.addStringOption(option =>
-	// 		option.setName('language')
-	// 			.setDescription('Search for the desired translation.')
-	// 			.setAutocomplete(true)),
-	// async autocomplete(interaction) {
-	// 	// handle the autocompletion response (more on how to do that below)
-	// 	const focusedOption = interaction.options.getFocused(true);
 
-	// 	let choices;
-
-	// 	if (focusedOption.name === 'language') {
-	// 		choices = translationsCodeDict;
-	// 	}
-	// 	// if (focusedOption.name === 'region') {
-	// 	// 	choices = countryDict;
-	// 	// }
-
-	// 	const filtered = choices.filter(choice => choice.name.toLowerCase().startsWith(focusedOption.value.toLowerCase()) || choice.value.toLowerCase().startsWith(focusedOption.value.toLowerCase())).slice(0, 25);
-	// 	await interaction.respond(
-	// 		filtered.map(choice => ({ name: `${choice.name} (${choice.value.toUpperCase()})`, value: choice.value })),
-	// 	);
-	// },
 	async execute(interaction) {
 
 
