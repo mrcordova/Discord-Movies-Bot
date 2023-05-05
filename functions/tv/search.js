0e9649ed90e98662d23ce4b4350274c70239887e
mrcordova <46950394+mrcordova@users.noreply.github.com>
2023-05-05 10:15:13 -0700

    remove dead comment tv search

--- functions/tv/search.js
+++ functions/tv/search.js
@@ -13,44 +13,7 @@ const tv_details = '/tv';
 
 
 module.exports = {
-	// data: new SlashCommandBuilder()
-	// 	.setName('tv-search')
-	// 	.setDescription('Search for tv show based on a text query.')
-	// 	.addStringOption(option =>
-	// 		option.setName('title')
-	// 			.setDescription('Search for the desired TV Show.')
-	// 			.setRequired(true))
-	// 	.addStringOption(option =>
-	// 		option.setName('language')
-	// 			.setDescription('Search for the desired translation.')
-	// 			.setAutocomplete(true))
-	// 	.addStringOption(option =>
-	// 		option.setName('region')
-	// 			.setDescription('Search for the desired region.')
-	// 			.setAutocomplete(true))
-	// 	.addIntegerOption(option =>
-	// 		option.setName('release-year')
-	// 			.setDescription('Search for the desired year.')
-	// 			.setMinValue(1800)
-	// 			.setMaxValue(3000)),
-	// async autocomplete(interaction) {
-	// 	// handle the autocompletion response (more on how to do that below)
-	// 	const focusedOption = interaction.options.getFocused(true);
 
-	// 	let choices;
-
-	// 	if (focusedOption.name === 'language') {
-	// 		choices = translationsCodeDict;
-	// 	}
-	// 	if (focusedOption.name === 'region') {
-	// 		choices = countryDict;
-	// 	}
-
-	// 	const filtered = choices.filter(choice => choice.name.toLowerCase().startsWith(focusedOption.value.toLowerCase()) || choice.value.toLowerCase().startsWith(focusedOption.value.toLowerCase())).slice(0, 25);
-	// 	await interaction.respond(
-	// 		filtered.map(choice => ({ name: `${choice.name} (${choice.value.toUpperCase()})`, value: choice.value })),
-	// 	);
-	// },
 	async execute(interaction) {
 
 		const query = interaction.options.getString('title');
