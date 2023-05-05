663741f09a808f104b126e7ea5911692c4194f02
mrcordova <46950394+mrcordova@users.noreply.github.com>
2023-05-05 10:05:36 -0700

    remove dead comments tv alt-titles

--- functions/tv/alt-titles.js
+++ functions/tv/alt-titles.js
@@ -24,33 +24,7 @@ const forwardButton = createButton('Next', ButtonStyle.Secondary, forwardId, 'âž
 
 
 module.exports = {
-	// data: new SlashCommandBuilder()
-	// 	.setName('tv-alt-titles')
-	// 	.setDescription('Get alternative titles for a tv show.')
-	// 	.addStringOption(option =>
-	// 		option.setName('title')
-	// 			.setDescription('Search for the desired TV Show.')
-	// 			.setRequired(true))
-	// 	.addStringOption(option =>
-	// 		option.setName('country')
-	// 			.setDescription('Search speific country.')
-	// 			.setAutocomplete(true)),
-	// async autocomplete(interaction) {
-	// 	// handle the autocompletion response (more on how to do that below)
-	// 	const focusedOption = interaction.options.getFocused(true);
 
-	// 	let choices;
-
-	// 	if (focusedOption.name === 'country') {
-	// 		choices = countryDict;
-	// 	}
-
-
-	// 	const filtered = choices.filter(choice => choice.name.toLowerCase().startsWith(focusedOption.value.toLowerCase()) || choice.value.toLowerCase().startsWith(focusedOption.value.toLowerCase())).slice(0, 25);
-	// 	await interaction.respond(
-	// 		filtered.map(choice => ({ name: `${choice.name} (${choice.value.toUpperCase()})`, value: choice.value })),
-	// 	);
-	// },
 	async execute(interaction) {
 
 		const query = interaction.options.getString('title');
